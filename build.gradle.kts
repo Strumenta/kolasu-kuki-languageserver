97ed1666fef1623c3bc94f0cbd18f328cc01d8b3
Federico Tomassetti <federico@strumenta.com>
2024-02-27 11:12:00 +0100

    Make tests logging more

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,21 @@ tasks.wrapper {
     gradleVersion = "8.6"
     distributionType = Wrapper.DistributionType.ALL
 }
+
+subprojects {
+    tasks.withType(Test::class).all {
+        testLogging {
+            showStandardStreams = true
+            showExceptions = true
+            exceptionFormat =
+                org
+                    .gradle
+                    .api
+                    .tasks
+                    .testing
+                    .logging
+                    .TestExceptionFormat
+                    .FULL
+        }
+    }
+}
