cf6a4235925caa21cb909b6b38e8d0193027b78a
Martin Azpillaga Aldalur <martin@minim.tools>
2024-06-11 20:01:07 +0200

    fix semantic token encoding bugs related to delta positioning

--- language-server/src/main/kotlin/com/strumenta/kuki/semanticHighlighting/SemanticHighlighting.kt
+++ language-server/src/main/kotlin/com/strumenta/kuki/semanticHighlighting/SemanticHighlighting.kt
@@ -3,22 +3,22 @@ package com.strumenta.kuki.semanticHighlighting
 import com.strumenta.kolasu.model.Position
 import org.eclipse.lsp4j.SemanticTokens
 
-data class SemanticToken (val position: Position, val type: SemanticTokenType, val modifiers: List<SemanticTokenModifier>)
+data class SemanticToken(val position: Position, val type: SemanticTokenType, val modifiers: List<SemanticTokenModifier>)
 
 fun encode(tokens: List<SemanticToken>): SemanticTokens {
-    var lastLine = 0
+    var lastLine = 1 // ? To offset that Kolasu lines start at 1
     var lastColumn = 0
     val data = mutableListOf<Int>()
     for (token in tokens) {
         data.addAll(listOf(
-            (token.position.start.line - 1) - lastLine,
-            token.position.start.column - lastColumn,
+            token.position.start.line - lastLine,
+            if (token.position.start.line == lastLine) token.position.start.column - lastColumn else token.position.start.column,
             token.position.end.column - token.position.start.column, // ! assumes tokens are in a single line
             token.type.ordinal,
             token.modifiers.sumOf { it.bit }
         ))
         lastLine = token.position.start.line
-        lastColumn = token.position.end.column
+        lastColumn = token.position.start.column
     }
     return SemanticTokens(data)
 }
