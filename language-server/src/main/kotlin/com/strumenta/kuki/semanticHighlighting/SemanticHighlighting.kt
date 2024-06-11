6a7edcfce549b1d66ab5bdf6ecb5510ed9422e42
Martin Azpillaga Aldalur <martin@minim.tools>
2024-06-11 18:24:54 +0200

    encode list of semantic tokens

---
+++ language-server/src/main/kotlin/com/strumenta/kuki/semanticHighlighting/SemanticHighlighting.kt
@@ -0,0 +1,63 @@
+package com.strumenta.kuki.semanticHighlighting
+
+import com.strumenta.kolasu.model.Position
+import org.eclipse.lsp4j.SemanticTokens
+
+data class SemanticToken (val position: Position, val type: SemanticTokenType, val modifiers: List<SemanticTokenModifier>)
+
+fun encode(tokens: List<SemanticToken>): SemanticTokens {
+    var lastLine = 0
+    var lastColumn = 0
+    val data = mutableListOf<Int>()
+    for (token in tokens) {
+        data.addAll(listOf(
+            (token.position.start.line - 1) - lastLine,
+            token.position.start.column - lastColumn,
+            token.position.end.column - token.position.start.column, // ! assumes tokens are in a single line
+            token.type.ordinal,
+            token.modifiers.sumOf { it.bit }
+        ))
+        lastLine = token.position.start.line
+        lastColumn = token.position.end.column
+    }
+    return SemanticTokens(data)
+}
+
+enum class SemanticTokenType (val legendName: String) {
+    NAMESPACE("namespace"),
+    CLASS("class"),
+    ENUM("enum"),
+    INTERFACE("interface"),
+    STRUCT("struct"),
+    TYPE_PARAMETER("typeParameter"),
+    TYPE("type"),
+    PARAMETER("parameter"),
+    VARIABLE("variable"),
+    PROPERTY("property"),
+    ENUM_MEMBER("enumMember"),
+    DECORATOR("decorator"),
+    EVENT("event"),
+    FUNCTION("function"),
+    METHOD("method"),
+    MACRO("macro"),
+    LABEL("label"),
+    COMMENT("comment"),
+    STRING("string"),
+    KEYWORD("keyword"),
+    NUMBER("number"),
+    REGULAR_EXPRESSION("regexp"),
+    OPERATOR("operator")
+}
+
+enum class SemanticTokenModifier(val legendName: String, val bit: Int) {
+    DECLARATION("declaration", 1),
+    DEFINITION("definition", 2),
+    READ_ONLY("readonly", 4),
+    STATIC("static", 8),
+    DEPRECATED("deprecated", 16),
+    ABSTRACT("abstract", 32),
+    ASYNCHRONOUS("async", 64),
+    MODIFICATION("modification", 128),
+    DOCUMENTATION("documentation", 256),
+    DEFAULT_LIBRARY("defaultLibrary", 512)
+}
