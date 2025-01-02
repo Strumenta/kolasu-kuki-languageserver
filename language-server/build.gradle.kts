7f5f6cf69ecc6724c1da51ea7b3b35bfc3e86977
Martin Azpillaga <martin@minim.tools>
2025-01-03 01:02:45 +0800

    load language server plugin from maven local version 1.0.6

--- language-server/build.gradle.kts
+++ language-server/build.gradle.kts
@@ -3,7 +3,11 @@ import java.nio.file.Files
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.8.22"
-    id("com.strumenta.kolasu.language-server-plugin") version "1.0.0"
+    id("com.strumenta.kolasu.language-server-plugin") version "1.0.6-SNAPSHOT"
+}
+
+repositories {
+    mavenLocal()
 }
 
 dependencies {
